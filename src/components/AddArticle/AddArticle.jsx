e1407b53fbd98a802b78e71110e27be5dda23669
Dmitry <dm.ovsienko@justiceteam-it.com>
2021-09-16 18:22:38 +0300

    50%

--- src/components/AddArticle/AddArticle.jsx
+++ src/components/AddArticle/AddArticle.jsx
@@ -1,16 +1,47 @@
 import React, {useState} from "react";
-import {EditorState, RichUtils} from 'draft-js';
-import 'draft-js/dist/Draft.css';
-import {Editor} from "react-draft-wysiwyg"
+import {EditorState} from 'draft-js';
+import {Editor} from "react-draft-wysiwyg";
+import {convertToHTML} from "draft-convert";
+
+import {ButtonAddArticle} from "./ButtonAddArticle/ButtonAddArticle";
 
 import "./AddArticle.scss";
-import {ButtonAddArticle} from "./ButtonAddArticle/ButtonAddArticle";
+import 'draft-js/dist/Draft.css';
+import convertFromHTML from "draft-convert/lib/convertFromHTML";
 
 export const AddArticle = () => {
 	const [editorState, setEditorState] = useState(
 		EditorState.createEmpty()
 	);
-	console.log('===>editorState', editorState.toJS());
+	// console.log('===>editorState.getCurrentContent()', editorState.getCurrentContent());
+	// console.log('===>editorState', editorState.toJS());
+	
+	const  html  =  convertToHTML(editorState.getCurrentContent());
+	const test = EditorState.createWithContent(convertFromHTML(html));
+	console.log('===>test', test);
+	// const html = convertToHTML({
+	// 	styleToHTML: (style) => {
+	// 		console.log('===>style', style);
+	// 		if (style === 'BOLD') {
+	// 			return <span style={{color: 'blue'}} />;
+	// 		}
+	// 	},
+	// 	blockToHTML: (block) => {
+	// 		if (block.type === 'PARAGRAPH') {
+	// 			return <p />;
+	// 		}
+	// 	},
+	// 	entityToHTML: (entity, originalText) => {
+	// 		if (entity.type === 'LINK') {
+	// 			return <a href={entity.data.url}>{originalText}</a>;
+	// 		}
+	// 		return originalText;
+	// 	}
+	// })(editorState.getCurrentContent());
+	console.log('===>html', html);
+	const handleValueInput = (e) => {
+		const {value, name} = e.target
+	}
 
 	return (
 		<div className='container'>
@@ -20,6 +51,20 @@ export const AddArticle = () => {
 				</div>
 				<div className='addArticle'>
 					<div className='descriptionArticle'>
+						<div className='inputBlock'>
+							<input
+								name='title'
+								type='text'
+								placeholder='Enter a title'
+								onChange={handleValueInput}
+							/>
+							<input
+								name='category'
+								type='text'
+								placeholder='Enter the category name...'
+								onChange={handleValueInput}
+							/>
+						</div>
 						<div className="editor">
 							<Editor
 								editorState={editorState}
@@ -39,9 +84,9 @@ export const AddArticle = () => {
 						<ButtonAddArticle text={'Publish an article'} />
 					</div>
 				</div>
-				<div className='viewHtml'>
-
-				</div>
+				<div className='viewHtml'
+						 dangerouslySetInnerHTML={{ __html: `${html}` }}
+				/>
 			</div>
 		</div>
 	)
